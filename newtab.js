55beedcd78a6c174228956ad1b1712b97520150e
Christina Hess <mchristinahess@gmail.com>
2016-08-17 18:21:33 +0000

    Create new tab page that renders iframe with website

---
+++ newtab.js
@@ -0,0 +1,12 @@
+// // var iconURL = chrome.extension.getURL("/icon.png");
+
+// document.addEventListener("DOMContentLoaded", function(event) {
+
+//     document.getElementById("iconURL").innerHTML = '<img src="/icon.png"/>';
+
+// })
+
+//use an iframe that points to the photo url
+// <iframe src=""/>
+// hardcode the id you are going to demo
+// make sure that you don't want
